e54be2c8d059473a42a2d19667b6234f91419a9c
nawal zakri <nawal.zakri1@gmail.com>
2017-12-16 12:21:17 +0100

    ville

--- CovoiturageEJB/ejbModule/META-INF/insert.sql
+++ CovoiturageEJB/ejbModule/META-INF/insert.sql
@@ -1,3 +1,26 @@
 insert into utilisateur (login,password) values ('admin','admin');
 insert into utilisateur (login,password) values   ('toto','toto');
 
+
+insert into  ville (idville ,labelville) VALUES (1,'Paris'); 
+insert into  ville (idville , labelville) VALUES (2,'Orléans') ;
+insert into  ville (idville ,labelville) VALUES (3,'Bourges') ; 
+insert into  ville (idville ,labelville) VALUES (4,'Lille') ; 
+insert into  ville (idville ,labelville) VALUES (5,'Toulouse') ;
+insert into  ville (idville ,labelville) VALUES (6,'Nantes'); 
+insert into  ville (idville ,labelville) VALUES (7,'Rennes') ; 
+insert into  ville (idville ,labelville) VALUES (8,'Bordeaux') ;  
+insert into  ville (idville ,labelville) VALUES (9,'Marseille') ; 
+insert into  ville (idville ,labelville) VALUES (10,'Strasbourg') ; 
+insert into  ville (idville ,labelville) VALUES (11,'Nice') ; 
+insert into  ville (idville ,labelville) VALUES (12,'Lyon') ;
+insert into  ville (idville ,labelville) VALUES (13,'Toulon'); 
+insert into  ville (idville ,labelville) VALUES (14,'Reims') ;
+insert into  ville (idville ,labelville) VALUES (15,'Montpellier') ;
+insert into  ville (idville ,labelville) VALUES (16,'Angers') ; 
+insert into  ville (idville ,labelville) VALUES (17,'Limoges') ;
+insert into  ville (idville ,labelville) VALUES (18,'Grenoble') ; 
+insert into  ville (idville ,labelville) VALUES (19,'Dijon') ; 
+insert into  ville (idville ,labelville) VALUES (20,'Caen') ;
+insert into  ville (idville ,labelville) VALUES (21,'Tours') ;
+insert into  ville (idville ,labelville) VALUES (22,'Clermont-Ferrand') ;
